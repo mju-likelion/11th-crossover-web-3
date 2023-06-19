efd6279e3358e812ccb23560a60198c359acefff
ahyexng <kay561834@gmail.com>
2023-06-19 20:35:59 +0900

    chore: app수정

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./styles/Theme";
-import Input from "./components/InputFilld";
+
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={Theme}>
         <GlobalStyle />
-        <Input />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </ThemeProvider>
     </div>
   );
