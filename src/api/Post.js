8c402eb822206cc91e416671931c14ff6bf167e8
ahyexng <kay561834@gmail.com>
2023-06-29 13:54:31 +0900

    feat: Delete api add

--- src/api/Post.js
+++ src/api/Post.js
@@ -17,8 +17,27 @@ export const AxiosWrite = (data, callbackFunctions) => {
   )
     .then((res) => {
       navigateSuccess();
+      window.localStorage.setItem("id", JSON.stringify(res.data.id));
+      console.log(res);
+      console.log("작성", res.data);
+      console.log(res.data.id);
     })
     .catch((error) => {
       error.response.data.message.map((message) => alert(message));
+    });
+};
+
+export const AxiosDelete = (callbackFunctions) => {
+  const { navigateSuccess } = callbackFunctions;
+  const id = JSON.parse(localStorage.getItem("id"));
+  Axios.delete(`/api/posts/${id}`)
+    .then((res) => {
+      navigateSuccess();
+      console.log(res);
+      alert("삭제");
+    })
+    .catch((error) => {
+      //   error.response.data.message.map((message) => alert(message));
+      alert("error", error);
     });
 };
